b5cc83cb1c3eb16cc81fa33b96c040bac5b92ffc
seetha-suresh13 <seetha.0706@gmail.com>
2025-07-17 18:36:47 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,14 @@
-locals {
-  region = "ap-south-1"
-  name   = "seetha-eks-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = local.region
 }
