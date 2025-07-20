7c87f623f3973a4541c20e2333cb238ef2e399d3
seetha-suresh13 <seetha.0706@gmail.com>
2025-07-20 14:28:57 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -44,32 +44,3 @@ module "eks" {
 
   tags = local.tags
 }
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
-
-  eks_managed_node_groups = {
-      cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
-    }
-  }
-
-  tags = local.tags
-}
