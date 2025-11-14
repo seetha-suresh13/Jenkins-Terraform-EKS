f486b9704c274305c63cbcc45cba58c2931803ae
seetha-suresh13 <seetha.0706@gmail.com>
2025-11-14 13:41:58 +0530

    Update local.tf

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,8 +1,8 @@
 locals {
-  region     = "ap-south-1"
+  region     = "us-east-1"
   name       = "my-eks-cluster"
   vpc_cidr   = "10.0.0.0/16"
-  azs        = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  azs        = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
