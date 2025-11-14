280ea31ac2c2a3d2bbde0b1f1a9a38d5d5734e5e
seetha-suresh13 <seetha.0706@gmail.com>
2025-11-14 13:49:50 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "22.0.0"
 
   cluster_name                    = local.name
   cluster_endpoint_public_access = true
